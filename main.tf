12db7d58da8f633b6affda3f362e4344d3329af8
mrmsx777 <elattariayoub1@gmail.com>
2025-10-04 15:49:48 +0100

    rem terraform fmt

--- main.tf
+++ main.tf
@@ -4,7 +4,6 @@ data "azurerm_client_config" "current" {}
 # Minimal resource to validate auth + backend + plan/apply
 data "azurerm_resource_group" "rg" {
   name     = local.rg_name
-  tags     = local.tags
 }
 
 
