f8a4f5a1a13eaabab43bc9a7d3576d0fb63cafdf
mrmsx777 <elattariayoub1@gmail.com>
2025-10-06 11:37:56 +0100

    +disk -performence

--- main.tf
+++ main.tf
@@ -33,6 +33,5 @@ resource "azurerm_managed_disk" "example" {
   storage_account_type = "Standard_LRS"
   create_option        = "Empty"
   disk_size_gb        = "32" 
-  performance_tier    = "Standard"
 
 }
