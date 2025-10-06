947eab737f52645d0ac4a8295d60175f8cf142c8
mrmsx777 <elattariayoub1@gmail.com>
2025-10-06 14:58:51 +0100

    provisioning vnets..

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ resource "azuread_group" "itadmins" {
   members          = [azuread_user.az104_user1.object_id]
 }
 
-
+#Example of map-object
 variable "disks" {
   type = map(object({
     name                 = string
@@ -54,4 +54,55 @@ resource "azurerm_managed_disk" "disks" {
   create_option        = "Empty"
   disk_size_gb        = "32" 
 
+}
+
+
+variable "vnets" {
+  type = map(object({
+    name          = string
+    address_space = list(string)  #< must be a list
+    subnet_name1  = string
+    subnet_prefix1 = string
+    subnet_name2  = string
+    subnet_prefix2 = string
+  }))
+
+  default = {
+    vnet1 = {
+      name           = "CoreServicesVnet"
+      address_space  = ["10.20.0.0/16"]       # always list
+      subnet_name1   = "SharedServicesSubnet"
+      subnet_prefix1 = "10.20.10.0/24"
+      subnet_name2   = "DatabaseSubnet"
+      subnet_prefix2 = "10.20.20.0/24"
+    }
+
+    vnet2 = {
+      name           = "ManufacturingVnet"
+      address_space  = ["10.30.0.0/16"]       
+      subnet_name1   = "SensorSubnet1"
+      subnet_prefix1 = "10.30.20.0/24"
+      subnet_name2   = "SensorSubnet2"
+      subnet_prefix2 = "10.30.21.0/24"
+    }
+  }
+}
+
+resource "azurerm_virtual_network" "vnets" {
+  for_each            = var.vnets
+  name                = each.value.name
+  location            = "East US"
+  resource_group_name = data.azurerm_resource_group.rg.name
+  address_space       = each.value.address_space  
+
+ 
+  subnet {
+    name           = each.value.subnet_name1
+    address_prefix = each.value.subnet_prefix1
+  }
+
+  subnet {
+    name           = each.value.subnet_name2
+    address_prefix = each.value.subnet_prefix2
+  }
 }
